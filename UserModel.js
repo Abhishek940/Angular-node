b75650233b1750fc6f181db8d3296cd6e2a7064f
Abhishek940 <74046993+Abhishek940@users.noreply.github.com>
2025-02-12 12:00:26 +0530

    forgtpassword

---
+++ UserModel.js
@@ -0,0 +1,54 @@
+const { string, required } = require('joi');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name: { 
+        type: String, 
+        required: true,
+    
+      },
+      email: { 
+        type: String,
+        required: true 
+    },
+      mobile: { 
+        type: Number, 
+        required: true 
+     },
+     password:{
+        type: String,
+        required:true
+     },
+     resetToken: {
+        type: String, 
+      },
+      resetTokenExpiration: {
+        type: Date,  
+      },
+   
+ 
+}, { timestamps: true });
+
+
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next(); // Skip if the password isn't modified
+    try {
+        const salt = await bcrypt.genSalt(10); // Generate salt with 10 rounds
+        this.password = await bcrypt.hash(this.password, salt); // Hash the password with the generated salt
+        next();
+    } catch (error) {
+        next(error); // Pass any error that occurs during hashing to the next middleware
+    }
+});
+
+// Define an instance method for comparing passwords
+userSchema.methods.comparePassword = async function (inputPassword) {
+    console.log('Password :', this.password); 
+    console.log('Password entered by user:', inputPassword);
+    return await bcrypt.compare(inputPassword, this.password); // Compare passwords
+};
+
+
+module.exports = mongoose.model('User', userSchema, 'users');
