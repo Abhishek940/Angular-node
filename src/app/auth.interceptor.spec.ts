d195bc9b70aa89e0e978ff7af14407dd29ccbc45
CSMCORP\abhishek.k <abhishek.k@csm.tech>
2026-09-01 11:41:50 +0530

    changes

--- src/app/auth.interceptor.spec.ts
+++ src/app/auth.interceptor.spec.ts
@@ -1,14 +1,15 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpInterceptorFn } from '@angular/common/http';
+import { AuthInterceptor } from './auth.interceptor';
 
-import { authInterceptor } from './auth.interceptor';
-
-describe('authInterceptor', () => {
-  const interceptor: HttpInterceptorFn = (req, next) => 
-    TestBed.runInInjectionContext(() => authInterceptor(req, next));
+describe('AuthInterceptor', () => {
+  let interceptor: AuthInterceptor;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [AuthInterceptor]
+    });
+
+    interceptor = TestBed.inject(AuthInterceptor);
   });
 
   it('should be created', () => {
