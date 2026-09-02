3d5935842aeec17d314fed61626025aa9da85438
CSMCORP\abhishek.k <abhishek.k@csm.tech>
2026-09-02 11:40:01 +0530

    URL

--- src/app/services/user-service.service.ts
+++ src/app/services/user-service.service.ts
@@ -6,7 +6,8 @@ import { Observable } from 'rxjs';
 })
 export class UserServiceService {
 
-  private apiUrl = 'http://localhost:3000/api';
+ // private apiUrl = 'http://localhost:3000/api';
+  private apiUrl = 'https://angular-node-67cz.onrender.com/api';
 
   constructor(private http: HttpClient) {}
 
